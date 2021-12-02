7a8fe3687ba60a3bed05257312c06777a8c591c9
alexander <allexandersokollov@gmail.com>
2021-12-02 08:54:08 +0300

    add ArrayUtils and refactored

--- src/main/java/cs/vsu/sokolov/Main.java
+++ src/main/java/cs/vsu/sokolov/Main.java
@@ -1,15 +1,15 @@
 package cs.vsu.sokolov;
 
-import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Scanner;
+
+
+
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
 
-        String massage = "Enter your sequence. ";
-        int[] seq = getIntArrFromConsole(massage);
+        int[] seq = ArrayUtils.readIntArrayFromConsole();
 
         int amount = SequenceAnalyzer.getAmountOfMonotoneDecreasingSubsequences(seq);
 
@@ -18,22 +18,4 @@ public class Main {
 
 
 
-    public static int[] getIntArrFromConsole(String massage) {
-        Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        System.out.println(massage + " To stop entering enter not an integer\n");
-
-        while (in.hasNextInt()) {
-            arr.add(in.nextInt());
-        }
-
-        int[] ints = new int[arr.size()];
-
-        for (int i = 0; i < ints.length; i++) {
-            ints[i] = arr.get(i);
-        }
-
-        return ints;
-    }
 }
--- src/main/java/cs/vsu/sokolov/SequenceAnalyzer.java
+++ src/main/java/cs/vsu/sokolov/SequenceAnalyzer.java
@@ -6,17 +6,15 @@ public class SequenceAnalyzer {
         int amountOfMonotoneDecreasingSubsequences = 0;
         boolean isPrevDecreasing = false;
 
-
         for (int i = 1; i < sequence.length; i++) {
-
-            if (sequence[i] < sequence[i - 1] && !isPrevDecreasing) {
-                amountOfMonotoneDecreasingSubsequences++;
-                isPrevDecreasing = true;
-
-            } else if (!(sequence[i] < sequence[i - 1])) {
+            if (sequence[i] < sequence[i - 1]) {
+                if (!isPrevDecreasing) {
+                    amountOfMonotoneDecreasingSubsequences++;
+                    isPrevDecreasing = true;
+                }
+            } else {
                 isPrevDecreasing = false;
             }
-
         }
 
         return amountOfMonotoneDecreasingSubsequences;
